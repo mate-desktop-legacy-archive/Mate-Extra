6637cff5f9e1d20157d1c0aada7cc903a6c9ab96
Stefano Karapetsas <stefano@karapetsas.com>
2011-12-11 12:19:42 +0100

    python-corba moved

--- python-mate-desktop/mateapplet/applet.py
+++
@@ -1,7 +0,0 @@
-import warnings
-warnings.warn("Module mate.applet is deprecated; "
-              "please import mateapplet instead",
-              DeprecationWarning)
-del warnings
-
-from mateapplet import *
